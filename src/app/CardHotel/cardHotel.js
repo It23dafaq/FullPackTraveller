fbc6cb0f851f214b6eaf475569891e8e93e1fb9c
Constantinos Foulidis <cfoulidis@ADDC.NETWORK.ARXNET>
2020-07-23 14:43:38 +0200

    adding another media query

--- src/app/CardHotel/cardHotel.js
+++ src/app/CardHotel/cardHotel.js
@@ -26,29 +26,17 @@ class CardHotel extends Component  {
  render(){
 
   return (
+    <>
     <div className="row justify-content-center">
-<div className="col-auto listing-block">
+<div className="col-auto">
     <div className="media">
         <div className="fav-box"><i className="fa fa-heart-o" aria-hidden="true"></i>
            </div>
           <img className="d-flex align-self-start" src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?h=350&auto=compress&cs=tinysrgb" alt="Generic placeholder image"/>
           <div className="media-body pl-3">
               <div className="stars"><span className="float-left"><i className="text-warning fa fa-star"></i></span></div>
-            <div className="price">$799,000<small>New York</small></div>
-            <div className="stats">
-                <span><i className="fa fa-arrows-alt"></i>1678 Sq ft</span>
-                <span><i className="fa fa-bath"></i>2 Beadrooms</span>
-            </div>
-            <div className="address">4062 Walnut Hill Drive
-        Cincinnati</div>
-          </div>
-        </div>
-    <div className="media">
-        <div className="fav-box"><i className="fa fa-heart-o" aria-hidden="true"></i>
-</div>
-          <img className="d-flex align-self-start" src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?h=350&auto=compress&cs=tinysrgb" alt="Generic placeholder image"/>
-          <div className="media-body pl-3">
-            <div className="price">$506,400<small>New York</small></div>
+            <div className="price">$799,000<small className="mr-1">New York </small></div>
+            <div><big>Excellent city hotel. Good for sightseeing with nearby parking areas. Great rooms and fantastic service. Awesome vibe.</big></div>
             <div className="stats">
                 <span><i className="fa fa-arrows-alt"></i>1678 Sq ft</span>
                 <span><i className="fa fa-bath"></i>2 Beadrooms</span>
@@ -59,6 +47,27 @@ class CardHotel extends Component  {
         </div>
         </div>
         </div>
+        <div className="row justify-content-center">
+    <div className="col-auto listing-block">
+        <div className="media">
+            <div className="fav-box"><i className="fa fa-heart-o" aria-hidden="true"></i>
+               </div>
+              <img className="d-flex align-self-start" src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?h=350&auto=compress&cs=tinysrgb" alt="Generic placeholder image"/>
+              <div className="media-body pl-3">
+                  <div className="stars"><span className="float-left"><i className="text-warning fa fa-star"></i></span></div>
+                <div className="price">$799,000<small className="mr-1">New York </small></div>
+                <div><big>Excellent city hotel. Good for sightseeing with nearby parking areas. Great rooms and fantastic service. Awesome vibe.</big></div>
+                <div className="stats">
+                    <span><i className="fa fa-arrows-alt"></i>1678 Sq ft</span>
+                    <span><i className="fa fa-bath"></i>2 Beadrooms</span>
+                </div>
+                <div className="address">4062 Walnut Hill Drive
+            Cincinnati</div>
+              </div>
+            </div>
+            </div>
+            </div>
+        </>
   );
  }
 }
