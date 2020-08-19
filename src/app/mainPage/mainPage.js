a25c75a85debdc4b579a0af0663b68fb844bb9d0
Constantinos Foulidis <cfoulidis@ADDC.NETWORK.ARXNET>
2020-08-19 18:23:47 +0200

    fixing nav menu adding helmet to project

--- src/app/mainPage/mainPage.js
+++ src/app/mainPage/mainPage.js
@@ -15,6 +15,7 @@ import LinearIndeterminate from '../Spinner/ProgressBar'
 import SearchIcon from '@material-ui/icons/Search';
 import ReactTooltip from "react-tooltip";
 import Countrys from "../../store/countrys"
+import {Helmet} from "react-helmet";
 
 
 class MainPage extends Component  {
@@ -172,15 +173,15 @@ class MainPage extends Component  {
   }
 
   return (
-    <div className=""  >
+    <div className="ml-5"  >
           <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
               <div className="container ">
                   <a className="navbar-brand js-scroll-trigger" href="#page-top">{this.props.name}</a>
-                  <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive1" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                       Menu
                       <i className="fas fa-bars"></i>
                   </button>
-                  <div className="collapse navbar-collapse" id="navbarResponsive">
+                  <div className="collapse navbar-collapse" id="navbarResponsive1">
                       <ul className="navbar-nav ml-auto">
                           <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Search</a></li>
                           <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
@@ -298,12 +299,11 @@ class MainPage extends Component  {
                   </div>
                 </div>
         </section>
+        <Helmet>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
           <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-          <script src="assets/mail/jqBootstrapValidation.js"></script>
-          <script src="assets/mail/contact_me.js"></script>
-          <script src="js/scripts.js"></script>
+        </Helmet>
 
 
      </div>
